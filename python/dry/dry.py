c4b86e708ce7d26114ac966704747f9ec0f14a9b
lev <lev@broadinstitute.org>
2016-02-29 18:42:56 -0500

    python/dry/dry.py: added filter_samples fcn

--- python/dry/dry.py
+++ python/dry/dry.py
@@ -1,28 +1,146 @@
 import numpy as np
-import gct
+import pandas as pd
+import in_out.gct as gct
+
+
+def determine_assay_type(prov_code):
+    """
+    Convert assay_type from string to numpy array and compare to a list of allowable entries.
+
+    Input
+        -prov_code: string
+    Output
+        -assay_type: numpy string array, length=1
+    """
+    allowed_assay_types = np.array(["PR1", "GR1", "DR1"], dtype=np.str)
+    assay_type = np.array(prov_code, dtype=np.str)
+
+    # Check that assay type is one we expect
+    if assay_type not in allowed_assay_types:
+        error_msg = ("The first entry of the provenance code should be the assay type. " +
+                     "Assay type not recognized. assay_type: {}")
+        raise Exception(error_msg.format(assay_type))
+
+    return assay_type
+
 
 def update_prov_code(new_entry, existing_prov_code):
+    """
+    Append entry to provenance code.
+
+    Input
+        -new_entry: string
+        -existing_prov_code: numpy string array
+    Output
+        -updated_prov_code : numpy string array
+    """
+    updated_prov_code = np.append(existing_prov_code, new_entry)
+    return updated_prov_code
+
+def parse_prov_code(prov_code):
+    # Not sure when I'll use this, but seems good to keep
+    # prov_code = "PR1+L2X+SF8+PF8.5"
+    # expected_out = np.array(["PR1", "L2X", "SF8", "PF8.5"], dtype=np.str)
+    # actual_out = dry.parse_prov_code(prov_code)
+    # self.assertTrue(np.array_equal(actual_out, expected_out),
+    #                 "Expected output: {}, Actual output: {}".format(expected_out, actual_out))
+    # # Split along + in case there are other entries in the prov_code already
+    # prov_code_chunks = np.array(prov_code.split("+"))
     pass
 
 
 def filter_samples(df, sample_pct_cutoff):
-    # Return df (potentially of different size than original df)
-    pass
+    """
+    Remove samples (i.e. columns) with more than sample_pct_cutoff NaN values.
+
+    Input
+        -df: pandas dataframe
+        -sample_pct_cutoff: number
+    Output
+        -out_df: pandas dataframe (potentially smaller than original df)
+    """
+    # Number of NaNs per sample
+    num_nans = df.isnull().sum()
+
+    # Number of probes
+    num_probes = df.shape[1]
+
+    # Percent NaNs per sample
+    pct_nans_per_sample = num_nans.divide(num_probes)
+
+    # Only return samples with fewer % of NaNs than the cutoff
+    out_df = df.loc[:, pct_nans_per_sample < sample_pct_cutoff]
+    return out_df.values
 
 
 def filter_probes(df, probe_pct_cutoff, probe_sd_cutoff):
-    # Identify rows manually labeled for rejection
-    # Return df (potentially of different size than original df)
-    pass
+    """
+    Remove probes (i.e. rows) with more than probe_pct_cutoff NaN values.
+    Also remove probes with standard deviation higher than probe_sd_cutoff.
+
+    Input
+        -df: pandas dataframe
+        -probe_pct_cutoff: number
+    Output
+        -out_df: pandas dataframe (potentially smaller than original df)
+    """
+
+    ### CONTINUE HERE. ###
+
+    # Number of NaNs per sample
+    num_nans = df.isnull().sum()
+
+    # Number of probes
+    num_probes = df.shape[1]
+
+    # Percent NaNs per sample
+    pct_nans_per_sample = num_nans.divide(num_probes)
+
+    # Only return samples with fewer % of NaNs than the cutoff
+    out_df = df.loc[:, pct_nans_per_sample < probe_pct_cutoff]
+    return out_df
+
+
+    # num_nans_other_way = df.isnull().sum(axis=1)
+    # print num_nans_other_way
+    #     # Number of NaNs per sample
+    # num_nans = df.isnull().sum()
+    #
+    # # Number of probes
+    # num_probes = df.shape[1]
+    #
+    # # Percent NaNs per sample
+    # pct_nans_per_sample = num_nans.divide(num_probes)
+    #
+    # # Only return samples with fewer % of NaNs than the cutoff
+    # out_df = df.loc[:, pct_nans_per_sample < sample_pct_cutoff]
+    # return out_df
+    #
+    #
+    # # Identify rows manually labeled for rejection
+    # # Return df (potentially of different size than original df)
+    # pass
+
+def manual_probe_rejection(df):
+    """
+    Also remove those probes that were manually selected for rejection.
+    :param df:
+    :return:
+    """
+
+
 
 def optimize_sample_balance(blah):
     pass
 
+
 def main():
-    gct_obj = gct.GCT("/Users/lev/code/PSP/python/functional_tests/test_p100.gct")
-    gct_obj.read(row_inds=range(100),col_inds=range(10))
-    data = gct_obj.matrix
-    print data
+    # gct_obj = gct.GCT("/Users/lev/code/PSP/python/functional_tests/test_p100.gct")
+    # print(gct_obj.version)
+    # gct_obj.read(row_inds=range(100),col_inds=range(10))
+    # data = gct_obj.matrix
+    # print(data)
+    pass
 
 
 # if __name__ == '__main__':
--- python/dry/test_dry.py
+++ python/dry/test_dry.py
@@ -1,31 +1,52 @@
 import unittest
 import logging
-import setup_logger as setup_logger
+import utils.setup_logger as setup_logger
+import numpy as np
+import pandas as pd
 import dry
+import os
+import in_out.gct as gct
 
 logger = logging.getLogger(setup_logger.LOGGER_NAME)
+functional_tests_path = "/Users/lev/code/PSP/python/functional_tests"
 
 class TestDry(unittest.TestCase):
+    def test_determine_assay_type(self):
+        prov_code = "GR1"
+        expected_out = np.array("GR1", dtype=np.str)
+        actual_out = dry.determine_assay_type(prov_code)
+        self.assertTrue(np.array_equal(actual_out, expected_out),
+                        "Expected output: {}, Actual output: {}".format(expected_out, actual_out))
+
     def test_update_prov_code(self):
-#         (new_entry, existing_prov_code):
-        pass
+        existing_prov_code = np.array(["PR1"])
+        new_entry = "L2X"
+        expected_out = np.array(["PR1", "L2X"])
+        actual_out = dry.update_prov_code(new_entry, existing_prov_code)
+        self.assertTrue(np.array_equal(actual_out, expected_out),
+                        "Expected output: {}, Actual output: {}".format(expected_out, actual_out))
 
-
-	def test_filter_samples(self):
-# 	    filter_samples(df, sample_pct_cutoff)
-	    # Return df (potentially of different size than original df)
+    def test_filter_samples(self):
+        df = pd.DataFrame(np.array([[0.5, 0.2, 0.1, 0.25],
+                                    [np.nan, 0.45, 0.2, -0.1],
+                                    [np.nan, 0.02, np.nan, 0.3]], dtype=float))
+        expected_out = np.array([[0.2, 0.1, 0.25],
+                                 [0.45, 0.2, -0.1],
+                                 [0.02, np.nan, 0.3]], dtype=float)
+        actual_out = dry.filter_samples(df, sample_pct_cutoff = 0.3)
+        self.assertTrue(np.allclose(actual_out, expected_out, equal_nan=True),
+                        "\nExpected output: {}, \nActual output: {}".format(expected_out, actual_out))
 
     def test_filter_probes(self):
-#         filter_probes(df, probe_pct_cutoff, probe_sd_cutoff)
+        # filter_probes(df, probe_pct_cutoff, probe_sd_cutoff)
         # Identify rows manually labeled for rejection
         # Return df (potentially of different size than original df)
         pass
 
-	def test_main(self):
-	    dry.main()
+    def test_main(self):
+        dry.main()
 
 
 if __name__ == "__main__":
-	setup_logger.setup(verbose=True)
-
-	unittest.main()
+    setup_logger.setup(verbose=True)
+    unittest.main()
