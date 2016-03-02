d70c3342e967a8357a0e3568a40e560d6d2bf568
lev <lev@broadinstitute.org>
2016-03-02 15:39:05 -0500

    python/dry/dry.py: added optimize_sample_balance, remove_sample_outliers, and function_to_optimize

--- python/dry/dry.py
+++ python/dry/dry.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import minimize_scalar
 import pandas as pd
 import in_out.gct as gct
 
@@ -37,6 +38,7 @@ def update_prov_code(new_entry, existing_prov_code):
     updated_prov_code = np.append(existing_prov_code, new_entry)
     return updated_prov_code
 
+
 def parse_prov_code(prov_code):
     # Not sure when I'll use this, but seems good to keep
     # prov_code = "PR1+L2X+SF8+PF8.5"
@@ -63,7 +65,7 @@ def filter_samples(df, sample_pct_cutoff):
     num_nans = df.isnull().sum()
 
     # Number of probes
-    num_probes = df.shape[1]
+    num_probes = df.shape[0]
 
     # Percent NaNs per sample
     pct_nans_per_sample = num_nans.divide(num_probes)
@@ -85,41 +87,25 @@ def filter_probes(df, probe_pct_cutoff, probe_sd_cutoff):
         -out_df: pandas dataframe (potentially smaller than original df)
     """
 
-    ### CONTINUE HERE. ###
+    # Number of NaNs per probe
+    num_nans = df.isnull().sum(axis=1)
 
-    # Number of NaNs per sample
-    num_nans = df.isnull().sum()
+    # Number of samples
+    num_samples = df.shape[1]
 
-    # Number of probes
-    num_probes = df.shape[1]
+    # Percent NaNs per probe
+    pct_nans_per_probe = num_nans.divide(num_samples)
 
-    # Percent NaNs per sample
-    pct_nans_per_sample = num_nans.divide(num_probes)
+    # Probe standard deviations
+    probe_sds = df.std(axis=1)
 
-    # Only return samples with fewer % of NaNs than the cutoff
-    out_df = df.loc[:, pct_nans_per_sample < probe_pct_cutoff]
+    # Only return probes with fewer % of NaNs than the cutoff
+    # and lower sd than the cutoff
+    probes_to_keep = ((pct_nans_per_probe < probe_pct_cutoff) &
+                      (probe_sds < probe_sd_cutoff))
+    out_df = df.loc[probes_to_keep, :]
     return out_df
 
-
-    # num_nans_other_way = df.isnull().sum(axis=1)
-    # print num_nans_other_way
-    #     # Number of NaNs per sample
-    # num_nans = df.isnull().sum()
-    #
-    # # Number of probes
-    # num_probes = df.shape[1]
-    #
-    # # Percent NaNs per sample
-    # pct_nans_per_sample = num_nans.divide(num_probes)
-    #
-    # # Only return samples with fewer % of NaNs than the cutoff
-    # out_df = df.loc[:, pct_nans_per_sample < sample_pct_cutoff]
-    # return out_df
-    #
-    #
-    # # Identify rows manually labeled for rejection
-    # # Return df (potentially of different size than original df)
-    # pass
 
 def manual_probe_rejection(df):
     """
@@ -127,11 +113,92 @@ def manual_probe_rejection(df):
     :param df:
     :return:
     """
-
-
-
-def optimize_sample_balance(blah):
+    # Extract metadata
+    # Only return probes marked TRUE
     pass
+
+
+def optimize_sample_balance(df, offset_bounds=(-7, 7)):
+    """
+    For each sample, perform optimization to determine offset. Then, apply offset to data.
+
+    Input
+        -df: pandas dataframe
+        -offset_bounds: tuple of floats, default=(-7, 7)
+    Output
+        -df_with_offsets: pandas dataframe
+        -optimized_distances: numpy array of length=num_samples
+    """
+    # Determine the median value for each probe
+    probe_medians = df.median(axis=1)
+
+    # Initialize optimization outputs
+    num_samples = df.shape[1]
+    optimized_offsets = np.zeros(num_samples, dtype=float)
+    optimized_distances = np.zeros(num_samples, dtype=float)
+    unoptimized_distances = np.zeros(num_samples, dtype=float)
+    success_bools = np.zeros(num_samples, dtype=bool)
+    # N.B. 0 is the same as False if you specify that dtype is boolean
+
+    # For each sample, perform optimization
+    for sample_ind in range(num_samples):
+        sample_vals = df.loc[:, sample_ind]
+
+        # Calculate unoptimized distances
+        unoptimized_distances[sample_ind] = function_to_optimize(0, sample_vals, probe_medians)
+
+        # Calculate optimized distances
+        optimization_result = minimize_scalar(function_to_optimize, args=(sample_vals, probe_medians),
+                                              method="bounded", bounds=offset_bounds)
+
+        # Return offset, optimized distance, and a flag indicating if the solution converged
+        optimized_offsets[sample_ind] = optimization_result.x
+        optimized_distances[sample_ind] = optimization_result.fun
+        success_bools[sample_ind] = optimization_result.success
+
+    # If any samples did not converge, display those sample indices
+    if ~np.all(success_bools):
+        print(("The following samples " +
+               "failed to converge during optimization: \n{}").format(df.loc[:, ~success_bools]))
+    # TO-DO(lev): Should we still apply these offsets?
+
+    # Apply the optimized offsets
+    df_with_offsets = df.add(optimized_offsets)
+
+    # Return the df with offsets applied and the optimized distances
+    return df_with_offsets, optimized_distances
+
+
+def function_to_optimize(offset, values, medians):
+    """
+    This function computes a sort of variance for each sample.
+
+    Input
+        -offset: float
+        -values: numpy array of floats
+        -medians: numpy array of floats
+    Output
+        -out: float
+    """
+    out = sum(np.square((offset + values) - medians))
+    return out
+
+
+def remove_sample_outliers(df, distances, sd_sample_outlier_cutoff):
+    """
+    Calculate sample outlier cutoff and remove outlier samples.
+
+    Input
+        -df: pandas dataframe
+        -distances: numpy array of floats
+        -sd_sample_outlier_cutoff: float
+    Output
+        -df: pandas dataframe (potentially smaller than original df)
+    """
+    cutoff = np.mean(distances) + np.multiply(np.std(distances), sd_sample_outlier_cutoff)
+
+    # Only return samples whose distance metric is lower than the cutoff
+    return df[:, distances < cutoff]
 
 
 def main():
@@ -140,6 +207,13 @@ def main():
     # gct_obj.read(row_inds=range(100),col_inds=range(10))
     # data = gct_obj.matrix
     # print(data)
+
+    # if args.optim:
+    #     (df, distances) = optimize_sample_balance(df, offset_bounds=(-7, 7))
+    # else:
+    #     (df, distances) = calculate_unoptim_distances(df)
+    #
+    # remove_sample_outliers(df, distances, sd_sample_outlier_cutoff=3)
     pass
 
 
--- python/dry/test_dry.py
+++ python/dry/test_dry.py
@@ -33,15 +33,52 @@ class TestDry(unittest.TestCase):
         expected_out = np.array([[0.2, 0.1, 0.25],
                                  [0.45, 0.2, -0.1],
                                  [0.02, np.nan, 0.3]], dtype=float)
-        actual_out = dry.filter_samples(df, sample_pct_cutoff = 0.3)
+        actual_out = dry.filter_samples(df, sample_pct_cutoff=0.4)
         self.assertTrue(np.allclose(actual_out, expected_out, equal_nan=True),
-                        "\nExpected output: {}, \nActual output: {}".format(expected_out, actual_out))
+                        "\nExpected output:\n{} \nActual output:\n{}".format(expected_out, actual_out))
 
     def test_filter_probes(self):
-        # filter_probes(df, probe_pct_cutoff, probe_sd_cutoff)
-        # Identify rows manually labeled for rejection
-        # Return df (potentially of different size than original df)
-        pass
+        df = pd.DataFrame(np.array([[10, 0.2, 0.1, 0.25],
+                                    [np.nan, 0.45, 0.2, -0.1],
+                                    [np.nan, 0.02, np.nan, 0.3]], dtype=float))
+        expected_out = np.array([np.nan, 0.45, 0.2, -0.1], dtype=float)
+        actual_out = dry.filter_probes(df, probe_pct_cutoff=0.3, probe_sd_cutoff=3)
+        self.assertTrue(np.allclose(actual_out.values, expected_out, equal_nan=True),
+                        "\nExpected output:\n{} \nActual output:\n{}".format(expected_out, actual_out.values))
+
+    def test_function_to_optimize(self):
+        offset = 0.5
+        values = np.array([1, 2.5, 3, 4, 5])
+        means = np.array([2, 0.5, 0.1, 1.1, 1.5])
+        expected_out = 45.62
+        actual_out = dry.function_to_optimize(offset, values, means)
+        self.assertTrue(np.isclose(actual_out, expected_out),
+                        "\nExpected output: {} \nActual output: {}".format(expected_out, actual_out))
+
+    def test_optimize_sample_balance(self):
+        df = pd.DataFrame(np.array([[10, -3, 1.2],
+                                    [0.45, 0.2, -0.1],
+                                    [4.5, -4, 0.3]], dtype=float))
+        expected_df_with_offsets = pd.DataFrame(np.array([[5.58, -0.16, 1.30],
+                                                          [-3.96, 3.03, 0],
+                                                          [0.08, -1.16, 0.40]], dtype=float))
+        expected_distances = np.array([36.62,  12.04,  0.06], dtype=float)
+        (actual_df_with_offsets, actual_distances) = dry.optimize_sample_balance(df)
+        self.assertTrue(np.allclose(actual_distances, expected_distances, atol=1e-2),
+                        "\nExpected output:\n{} \nActual output:\n{}".format(expected_distances,
+                                                                             actual_distances))
+        self.assertTrue(np.allclose(actual_df_with_offsets, expected_df_with_offsets, atol=1e-2),
+                        "\nExpected output:\n{} \nActual output:\n{}".format(expected_df_with_offsets,
+                                                                             actual_df_with_offsets))
+
+    def test_remove_sample_outliers(self):
+        # Haven't been able to create an example of an outlier sample
+        df = pd.DataFrame(np.array([[2.4, -0.1, 5.41],
+                                    [-1, 0.3, 0.20],
+                                    [1.5, -1.2, 0.40]], dtype=float))
+        expected_df = df
+        (_, distances) = dry.optimize_sample_balance(df)
+        dry.remove_sample_outliers(df, distances, sd_sample_outlier_cutoff=2)
 
     def test_main(self):
         dry.main()
