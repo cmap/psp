ddad1951100e09e60d2c973f89f43edf39beaee3
lev <lev@broadinstitute.org>
2016-05-11 16:31:38 -0400

    utils/tear.py: wrote level 3-4 module

---
+++ utils/tear.py
@@ -0,0 +1,104 @@
+import logging
+import utils.setup_logger as setup_logger
+import argparse
+import sys
+import dry.dry as dry
+
+
+__author__ = "Lev Litichevskiy"
+__email__ = "lev@broadinstitute.org"
+
+""" Z-scores data.
+
+Converts level 3 to level 4 data. Required input is a filepath to a gct file and
+a filepath to an output file. Output is writing a processed gct file.
+
+TODO(lev): borrows functionality from dry. Might consider saving common methods
+elsewhere... or combining with dry.
+
+N.B. This script requires a configuration file. You can specify the location
+of this config file with the optional argument -psp_config_path. Otherwise,
+it will look for the example configuration file (example_psp.cfg) in the
+current directory.
+
+TODO(lev) --> Example usage:
+
+"""
+
+ZSCORE_PROV_CODE_ENTRY = "ZSC"
+
+# Setup logger
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+
+def build_parser():
+    """Build argument parser."""
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # Required args
+    parser.add_argument("gct_path", type=str, help="filepath to gct file")
+    parser.add_argument("out_path", type=str, help="path to save directory")
+
+    # Optional args
+    parser.add_argument("-out_name", type=str, default=None,
+                        help="name of output gct (if None, will use <INPUT_GCT>.tear.processed.gct")
+    parser.add_argument("-psp_config_path", type=str,
+                        default="example_psp.cfg",
+                        help="filepath to PSP config file")
+    parser.add_argument("-verbose", "-v", action="store_true", default=False,
+                        help="increase the number of messages reported")
+
+    return parser
+
+
+def main(args):
+    # Read gct and config file
+    (in_gct, _, prov_code, config_io, config_metadata, _) = (
+        dry.read_gct_and_config_file(
+            args.gct_path, args.psp_config_path, None))
+
+    # Robust z-score
+    zscored_df = robust_zscore(in_gct.data_df)
+    (zscored_gct, updated_prov_code) = dry.update_metadata_and_prov_code(
+        zscored_df, in_gct.row_metadata_df, in_gct.col_metadata_df,
+        ZSCORE_PROV_CODE_ENTRY, prov_code)
+
+    # Configure output name
+    (out_gct_name, _) = dry.configure_out_names(args.gct_path, args.out_name, None)
+
+    # Reinsert provenance code
+    out_gct = dry.insert_offsets_and_prov_code(
+        zscored_gct, None, None, updated_prov_code,
+        config_metadata["prov_code_field"], config_metadata["prov_code_delimiter"])
+
+    # Write output gct
+    dry.write_output_gct(out_gct, args.out_path, out_gct_name,
+                         config_io["data_null"], config_io["filler_null"])
+    return out_gct
+
+
+def robust_zscore(data_df):
+    """Normalize data using robust z-score.
+
+    Formula: x' = (x - row_median) / (row_mad * 1.4826)
+
+    Args:
+        data_df (pandas df)
+    Returns:
+        out_df (pandas df): normalized
+    """
+    row_medians = data_df.median(axis=1)
+    row_mads = (data_df.sub(row_medians, axis=0)).abs().median(axis=1)
+    out_df = data_df.sub(row_medians, axis=0).divide(
+        1.4826 * row_mads, axis='index')
+    return out_df
+
+
+if __name__ == "__main__":
+    args = build_parser().parse_args(sys.argv[1:])
+    setup_logger.setup(verbose=args.verbose)
+    logger.debug("args: {}".format(args))
+
+    main(args)
---
+++ utils/test_tear.py
@@ -0,0 +1,48 @@
+import unittest
+import logging
+import utils.setup_logger as setup_logger
+import os
+import numpy as np
+import pandas as pd
+import tear
+
+# Setup logger
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+# Use functional tests assets from the dry directory
+FUNCTIONAL_TESTS_DIR = "dry/functional_tests"
+
+class TestRobustZscore(unittest.TestCase):
+    def test_robust_zscore(self):
+        my_df = pd.DataFrame([[0.3,0.2,0.8,0.7],
+                             [0.1,-0.2,0.1,0.9],
+                             [1.1,-0.3,np.nan,0.4]])
+        e_df = pd.DataFrame([[-0.54,-0.81,0.81,0.54],
+                             [0.0,-1.35,0.0,3.60],
+                             [0.67,-0.67,np.nan,0.0]])
+
+        out_df = tear.robust_zscore(my_df)
+        self.assertTrue(np.allclose(out_df, e_df, atol=1e-2, equal_nan=True))
+
+    def test_main(self):
+        INPUT_GCT_PATH = os.path.join(FUNCTIONAL_TESTS_DIR, "p100_prm_plate29_3H.gct")
+        OUT_NAME = "test_tear_p100_output.gct"
+
+        args_string = ("{} {} -out_name {}").format(
+            INPUT_GCT_PATH, FUNCTIONAL_TESTS_DIR, OUT_NAME)
+        args = tear.build_parser().parse_args(args_string.split())
+        out_gct = tear.main(args)
+
+        # print
+
+        self.assertAlmostEqual(out_gct.data_df.iloc[0,0], -1.20, places=2)
+        self.assertEqual(out_gct.col_metadata_df["provenance_code"].iloc[0],
+                         "PRM+L2X+ZSC")
+
+        # Clean up
+        os.remove(os.path.join(FUNCTIONAL_TESTS_DIR, OUT_NAME))
+
+
+if __name__ == "__main__":
+    setup_logger.setup()
+    unittest.main()
