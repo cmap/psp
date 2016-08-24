6b1aae5d53bdb9082fe68369e0b56c0a6ac890e1
lev <lev@broadinstitute.org>
2016-08-24 15:32:39 -0400

    dry/test_dry_against_R_code.py: script to programmatically compare dry to R code (in dev)

--- dry/test_dry_against_R_code.py
+++ dry/test_dry_against_R_code.py
@@ -1,167 +1,113 @@
-import unittest
-import logging
-import utils.setup_logger as setup_logger
+import glob
 import os
 import numpy as np
-import pandas as pd
+import logging
 
-import parse_gctoo
-import dry
-
-"""
-This code should be run from broadinstitute.psp.
-
-The dry directory contains a directory called functional_tests
-that has the assets required for the 3 functional tests below. For
-functional tests, I just check that they run to completion.
+import parse_gctoo as pg
+import utils.setup_logger as setup_logger
 
 """
+
+This script programmatically compares the output of dry to the processed gcts
+pulled down from Panorama.
+
+"""
+
+# Location of gcts processed using R code
+gct_loc_r_code = "/cmap/data/proteomics/produced_by_jjaffe_code/dry/wget_processed/"
+gct_r_code_suffix = "*P100*.gct"
+
+# Location of gcts processed using dry
+gct_loc_dry = "/cmap/data/proteomics/dry/"
+
+# Suffix for files created with dry
+dry_suffix = ".gct.dry.processed.gct"
+
+##########
+
 # Setup logger
 logger = logging.getLogger(setup_logger.LOGGER_NAME)
+setup_logger.setup(verbose=True)
 
-# Functional tests dir lives within the dry directory
-FUNCTIONAL_TESTS_DIR = "dry/functional_tests"
+# Get R files
+all_r_files = glob.glob(gct_loc_r_code + gct_r_code_suffix)
+assert len(all_r_files) > 1, "No R files were found!"
+logger.info("{} R files found".format(len(all_r_files)))
 
-# Set to false if you want to see what output is created
-CLEANUP = True
+# For each R file, see if we can find its complementary file created with dry
+# Only keep the R files with a complement
+r_files = []
+dry_files = []
 
-# N.B. e_out is expected_output.
-class TestDryAgainstRCode(unittest.TestCase):
+for r_file in all_r_files:
+    dry_file_path = os.path.join(gct_loc_dry, os.path.basename(r_file).split(".processed.gct")[0] + dry_suffix)
 
-    def test_p100_main(self):
-        INPUT_GCT_PATH = os.path.join(FUNCTIONAL_TESTS_DIR, "p100_prm_plate29_3H.gct")
-        JJ_OUTPUT_GCT = os.path.join(FUNCTIONAL_TESTS_DIR, "JJ_p100_prm_plate29_3H_processed.gct")
-        OUT_NAME = "test_dry_p100_output.gct"
-        OUT_PW_NAME = "test_dry_p100_output.pw"
+    if os.path.exists(dry_file_path):
+        dry_files.append(dry_file_path)
+        r_files.append(r_file)
+    else:
+        logger.debug("This dry file could not be found. dry_file_path: {}".format(
+            dry_file_path))
 
-        args_string = ("{} {} " +
-                       "-out_name {} " +
-                       "-out_pw_name {} " +
-                       "-sample_nan_thresh {} " +
-                       "-probe_nan_thresh {} " +
-                       "-dist_sd_cutoff {} " +
-                       "-v").format(INPUT_GCT_PATH, FUNCTIONAL_TESTS_DIR,
-                                    OUT_NAME, OUT_PW_NAME, 0.8, 0.9, 3)
-        args = dry.build_parser().parse_args(args_string.split())
-        out_gct = dry.main(args)
+# Do the comparison
+failed_comparisons = []
+for dry_file, r_file in zip(dry_files, r_files):
 
-        # Compare to output gct created by JJ's code
-        e_gct = parse_gctoo.parse(JJ_OUTPUT_GCT)
+    dry_gct = pg.parse(dry_file)
+    r_gct = pg.parse(r_file)
 
-        self.assertTrue(np.allclose(out_gct.data_df, e_gct.data_df, atol=1e-1, equal_nan=True),
-                        "The data_df that was returned is wrong.")
-        self.assertTrue(np.array_equal(e_gct.row_metadata_df, out_gct.row_metadata_df),
-                         "The row_metadata_df that was returned is wrong.")
-        # Column metadata should have one more header
-        self.assertEqual(e_gct.col_metadata_df.shape[1] + 1, out_gct.col_metadata_df.shape[1],
-                         ("Actual col_metadata_df should have one more header" +
-                          "than e_col_metadata_df.\n" +
-                          "e_gct.col_metadata_df.shape: {}, " +
-                          "out_gct.col_metadata_df.shape: {}").format(e_gct.col_metadata_df.shape,
-                                                                      out_gct.col_metadata_df.shape[1]))
+    logger.info("dry_file: {}".format(dry_file))
+    logger.info("r_file: {}".format(r_file))
 
-        # Check that column metadata is correct (ignore last 3 entries so that we can skip provenance code)
-        self.assertTrue(np.array_equal(e_gct.col_metadata_df.iloc[:, :-2], out_gct.col_metadata_df.iloc[:, :-3]),
-                        "The col_metadata_df that was returned is wrong.")
+    assert dry_gct.data_df.shape == r_gct.data_df.shape, "Shape of data_df is wrong!"
 
-        logger.info("Mean difference between gcts: {}".format(np.nanmean(e_gct.data_df.values - out_gct.data_df.values)))
-        logger.info("Mean absolute difference between gcts: {}".format(np.nanmean(np.absolute(e_gct.data_df.values - out_gct.data_df.values))))
+    try:
 
-        # Clean up
-        if CLEANUP:
-            os.remove(os.path.join(FUNCTIONAL_TESTS_DIR, OUT_NAME))
-            os.remove(os.path.join(FUNCTIONAL_TESTS_DIR, OUT_PW_NAME))
+        # Check data in 2 stages
+        strict_atol = 0.1
+        lenient_atol = 1
+        try:
+            # More strict
+            assert np.allclose(dry_gct.data_df, r_gct.data_df, atol=strict_atol, equal_nan=True), (
+                "data_df does not match up.")
 
-    def test_GCP_main(self):
-        INPUT_GCT_PATH = os.path.join(FUNCTIONAL_TESTS_DIR, "gcp_gr1_plate31.gct")
-        JJ_OUTPUT_GCT = os.path.join(FUNCTIONAL_TESTS_DIR, "JJ_gcp_gr1_plate31_processed.gct")
-        OUT_NAME = "test_dry_gcp_output.gct"
-        OUT_PW_NAME = "test_dry_gcp_output.pw"
+        except:
 
-        args_string = ("{} {} " +
-                       "-out_name {} " +
-                       "-out_pw_name {} " +
-                       "-sample_nan_thresh {} " +
-                       "-probe_nan_thresh {} " +
-                       "-probe_sd_cutoff {} " +
-                       "-v").format(INPUT_GCT_PATH, FUNCTIONAL_TESTS_DIR,
-                                    OUT_NAME, OUT_PW_NAME, 0.5, 0.5, 4)
-        args = dry.build_parser().parse_args(args_string.split())
-        out_gct = dry.main(args)
+            # Return number of differences above strict_atol
+            differences = dry_gct.data_df.subtract(r_gct.data_df).values.flatten()
+            logger.warning("number of differences > {}: {}".format(strict_atol, (differences > strict_atol).sum()))
 
-        # Compare to output gct created by JJ's code
-        e_gct = parse_gctoo.parse(JJ_OUTPUT_GCT)
-
-        self.assertTrue(np.allclose(out_gct.data_df, e_gct.data_df, atol=1e-1, equal_nan=True),
-                        "The data_df that was returned is wrong.")
-        self.assertTrue(np.array_equal(e_gct.row_metadata_df, out_gct.row_metadata_df),
-                        "The row_metadata_df that was returned is wrong.")
-
-        # Check that column metadata is correct (ignore last 2 entries so that we can skip provenance code)
-        self.assertTrue(np.array_equal(e_gct.col_metadata_df.iloc[:, :-2], out_gct.col_metadata_df.iloc[:, :-2]),
-                        "The col_metadata_df that was returned is wrong.")
-
-        logger.info("Mean difference between gcts: {}".format(np.nanmean(e_gct.data_df.values - out_gct.data_df.values)))
-        logger.info("Mean absolute difference between gcts: {}".format(np.nanmean(np.absolute(e_gct.data_df.values - out_gct.data_df.values))))
-
-        # No samples should have been filtered out
-        out_pw_df = pd.read_csv(
-            os.path.join(FUNCTIONAL_TESTS_DIR, OUT_PW_NAME), sep="\t")
-        self.assertTrue(all(out_pw_df["remains_after_poor_coverage_filtration"]))
-
-        # Clean up
-        if CLEANUP:
-            os.remove(os.path.join(FUNCTIONAL_TESTS_DIR, OUT_NAME))
-            os.remove(os.path.join(FUNCTIONAL_TESTS_DIR, OUT_PW_NAME))
+            # Less strict (absolute tolerance of 1)
+            assert np.allclose(dry_gct.data_df, r_gct.data_df, atol=lenient_atol, equal_nan=True), (
+                "data_df STILL does not match up!")
 
 
-    def test_p100_subset_main(self):
-        INPUT_GCT_PATH = os.path.join(FUNCTIONAL_TESTS_DIR, "p100_prm_plate35_subsets.gct")
-        JJ_OUTPUT_GCT = os.path.join(FUNCTIONAL_TESTS_DIR, "JJ_p100_prm_plate35_subsets_processed.gct")
-        OUT_NAME = "test_dry_p100_subsets_output.gct"
-        OUT_PW_NAME = "test_dry_p100_subsets_output.pw"
+        # Check col metadata
+        if "P100" in dry_file:
+            # P100 column metadata should have one more header
+            assert dry_gct.col_metadata_df.shape[1] == r_gct.col_metadata_df.shape[1] + 1, (
+                "dry_gct.col_metadata_df.shape: {}, r_gct.col_metadata_df.shape: {}").format(
+                dry_gct.col_metadata_df.shape, r_gct.col_metadata_df.shape[1])
 
-        args_string = ("{} {} " +
-                       "-out_name {} " +
-                       "-out_pw_name {} " +
-                       "-sample_nan_thresh {} " +
-                       "-probe_nan_thresh {} " +
-                       "-probe_sd_cutoff {} " +
-                       "-dist_sd_cutoff {} " +
-                       "-v").format(INPUT_GCT_PATH, FUNCTIONAL_TESTS_DIR,
-                                    OUT_NAME, OUT_PW_NAME, 0.8, 0.9, 3, 3)
-        args = dry.build_parser().parse_args(args_string.split())
-        out_gct = dry.main(args)
+            assert dry_gct.col_metadata_df.iloc[:, :-1].equals(r_gct.col_metadata_df), (
+                "col_metadata_df does not match up.")
 
-        # Compare to output gct created by JJ's code
-        e_gct = parse_gctoo.parse(JJ_OUTPUT_GCT)
+        else:
+            assert dry_gct.col_metadata_df.equals(r_gct.col_metadata_df), (
+                "col_metadata_df does not match up.")
 
-        self.assertTrue(np.allclose(out_gct.data_df, e_gct.data_df, atol=1e-1, equal_nan=True),
-                        "The data_df that was returned is wrong.")
-        self.assertTrue(np.array_equal(e_gct.row_metadata_df, out_gct.row_metadata_df),
-                         "The row_metadata_df that was returned is wrong.")
+        # Check row metadata
+        assert dry_gct.row_metadata_df.equals(r_gct.row_metadata_df), (
+            "row_metadata_df does not match up.")
 
-        # Column metadata should have one more header
-        self.assertEqual(e_gct.col_metadata_df.shape[1] + 1, out_gct.col_metadata_df.shape[1],
-                         ("Actual col_metadata_df should have one more header" +
-                          "than e_col_metadata_df.\n" +
-                          "e_gct.col_metadata_df.shape: {}, " +
-                          "out_gct.col_metadata_df.shape: {}").format(e_gct.col_metadata_df.shape,
-                                                                      out_gct.col_metadata_df.shape[1]))
+    except:
+        failed_comparisons.append(dry_file)
 
-        # Check that column metadata is correct for all but the new header and provenance code
-        self.assertTrue(np.array_equal(e_gct.col_metadata_df.iloc[:, :-1], out_gct.col_metadata_df.iloc[:, :-2]),
-                        "The col_metadata_df that was returned is wrong.")
+# Return summary result
+if len(failed_comparisons) == 0:
+    logger.info("Yay! All files matched.")
 
-        logger.info("Mean difference between gcts: {}".format(np.nanmean(e_gct.data_df.values - out_gct.data_df.values)))
-        logger.info("Mean absolute difference between gcts: {}".format(np.nanmean(np.absolute(e_gct.data_df.values - out_gct.data_df.values))))
-
-        # Clean up
-        if CLEANUP:
-            os.remove(os.path.join(FUNCTIONAL_TESTS_DIR, OUT_NAME))
-            os.remove(os.path.join(FUNCTIONAL_TESTS_DIR, OUT_PW_NAME))
-
-
-if __name__ == "__main__":
-    setup_logger.setup(verbose=True)
-    unittest.main()
+else:
+    logger.warning("The following {} files did not match up.".format(len(failed_comparisons)))
+    for fail in failed_comparisons:
+        print fail
