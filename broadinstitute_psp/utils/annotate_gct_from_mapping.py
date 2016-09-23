ee319626b2929b158a5cdcd776802e2689457bbd
lev <lev@broadinstitute.org>
2016-09-23 16:40:57 -0400

    utils/annotate_gct_from_mapping.py: script to annotate a gct using a mapping tsv file

---
+++ broadinstitute_psp/utils/annotate_gct_from_mapping.py
@@ -0,0 +1,113 @@
+"""
+annotate_gct_from_mapping.py
+
+Add annotations to a gct file using a mapping tsv file that looks like this:
+
+from_field  to_field1   to_field2
+pert1       moa1        target1
+pert2       moa2        target2
+
+This script will add "to_field1" and "to_field2" as metadata fields and populate
+those fields using the given mapping. "from_field" in the tsv will be mapped to
+the argument "gct_from_field", which must already be in the gct file. Note that
+"from_field" in the mapping tsv file is not used.
+
+"""
+
+import logging
+import sys
+import argparse
+import pandas as pd
+
+import broadinstitute_psp.utils.setup_logger as setup_logger
+import cmap.io.GCToo.parse_gctoo as pg
+import cmap.io.GCToo.write_gctoo as wg
+
+__author__ = "Lev Litichevskiy"
+__email__ = "lev@broadinstitute.org"
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+
+def build_parser():
+    """Build argument parser."""
+
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # Required args
+    parser.add_argument("--path_to_gct", "-i", required=True,
+                        help="path to dictionary tsv")
+    parser.add_argument("--path_to_mapping_tsv", "-m", required=True,
+                        help="path to mapping tsv")
+
+    # Optional args
+    parser.add_argument("--out_name", "-o", default="annotated.gct",
+                        help="what to name the annotated output gct")
+    parser.add_argument("--row_and_or_col", "-rc", choices=["row", "col", "both"],
+                        default="both",
+                        help=("whether to apply the mapping to only the row metadata, " +
+                              "only the col metadata, or both"))
+    parser.add_argument("--gct_from_field", "-f", default="pert_iname",
+                        help="field already in the gct file")
+    parser.add_argument("--missing_entry", "-me", default="NA",
+                        help="what to put for an entry if it doesn't have a mapping")
+    parser.add_argument("--verbose", "-v", action="store_true", default=False,
+                        help="whether to increase the # of messages reported")
+
+    return parser
+
+
+def main(args):
+
+    # Parse gct file
+    gct = pg.parse(args.path_to_gct)
+
+    # Parse mapping tsv file
+    mapping = pd.read_csv(args.path_to_mapping_tsv, sep="\t", index_col=0)
+
+    for col in mapping.columns:
+
+        if args.row_and_or_col == "both":
+            annotate_meta_df(gct.row_metadata_df, mapping.loc[:, col], args.gct_from_field, args.missing_entry)
+            annotate_meta_df(gct.col_metadata_df, mapping.loc[:, col], args.gct_from_field, args.missing_entry)
+        elif args.row_and_or_col == "row":
+            annotate_meta_df(gct.row_metadata_df, mapping.loc[:, col], args.gct_from_field, args.missing_entry)
+        elif args.row_and_or_col == "col":
+            annotate_meta_df(gct.row_metadata_df, mapping.loc[:, col], args.gct_from_field, args.missing_entry)
+
+    wg.write(gct, args.out_name, filler_null="NA", data_null="NaN", metadata_null="NA")
+
+
+def annotate_meta_df(meta_df, to_entries, gct_from_field, missing_entry):
+    """ meta_df is modified in-place.
+
+    Args:
+        meta_df (pandas df)
+        to_entries (pandas series)
+        gct_from_field (string): name of field in gct that has the source entries
+        missing_entry (string)
+
+    Returns:
+        None
+
+    """
+    assert gct_from_field in meta_df.columns, ("gct_from_field must be a metadata header. " +
+        "meta_df.columns.values: {}, gct_from_field: {}".format(
+        meta_df.columns.values, gct_from_field))
+
+    # Create mapped_metadata as an array
+    mapped_metadata = [to_entries.loc[entry_in_gct] if entry_in_gct in to_entries.index else missing_entry for entry_in_gct in meta_df.loc[:, gct_from_field]]
+
+    # Insert destination_metadata into row and col metadata
+    meta_df[to_entries.name] = mapped_metadata
+
+    return None
+
+
+
+if __name__ == "__main__":
+    args = build_parser().parse_args(sys.argv[1:])
+    setup_logger.setup(verbose=args.verbose)
+
+    main(args)
---
+++ broadinstitute_psp/utils/test_annotate_gct_from_mapping.py
@@ -0,0 +1,55 @@
+import unittest
+import os
+import pandas as pd
+
+import cmap.io.GCToo.parse_gctoo as pg
+import annotate_gct_from_mapping as agfm
+
+functional_tests_dir = "utils/functional_tests/"
+
+class TestAnnotate(unittest.TestCase):
+
+    def test_main(self):
+        gct_path = os.path.join(functional_tests_dir, "test_annotate_gct_from_mapping_in.gct")
+        mapping_path = os.path.join(functional_tests_dir, "test_annotate_gct_from_mapping.tsv")
+        expected_gct_path = os.path.join(functional_tests_dir, "test_annotate_gct_from_mapping_expected.gct")
+        out_path = os.path.join(functional_tests_dir, "test_annotate_gct_from_mapping_out.gct")
+
+        args_string = "-i {} -m {} -o {}".format(
+            gct_path, mapping_path, out_path)
+        args = agfm.build_parser().parse_args(args_string.split())
+
+        agfm.main(args)
+
+        # Read in expected and actual outputs
+        e_gct = pg.parse(expected_gct_path)
+        out_gct = pg.parse(out_path)
+
+        pd.util.testing.assert_frame_equal(e_gct.data_df, out_gct.data_df)
+        pd.util.testing.assert_frame_equal(e_gct.row_metadata_df, out_gct.row_metadata_df)
+        pd.util.testing.assert_frame_equal(e_gct.col_metadata_df, out_gct.col_metadata_df)
+
+    def test_annotate_meta_df(self):
+        meta_df = pd.DataFrame(
+            [["a"], ["b"], ["c"]], index=["A", "B", "C"], columns=["pert_iname"])
+        to_entries = pd.Series(
+            ["inhibitor", "activator", "killer", "winner"],
+            index=["f", "b", "c", "d"])
+        to_entries.name = "moa"
+
+        e_meta_df = pd.DataFrame(
+            [["a", "NA"], ["b", "activator"], ["c", "killer"]],
+            index=["A", "B", "C"], columns=["pert_iname", "moa"])
+
+        agfm.annotate_meta_df(meta_df, to_entries, "pert_iname", "NA")
+        pd.util.testing.assert_frame_equal(meta_df, e_meta_df)
+
+        with self.assertRaises(AssertionError) as e:
+            agfm.annotate_meta_df(meta_df, to_entries, "thing3", "NA")
+        self.assertIn("gct_from_field must be a metadata header", str(e.exception))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
