9b173a1762db7dc11f8ad9993267443d83a27fcd
lev <lev@broadinstitute.org>
2017-01-18 14:53:58 -0500

    steep_and_sip_external/steep_and_sip_external_many.py: improved stacktrace that is returned; added functional test and asset

--- broadinstitute_psp/steep_and_sip_external/steep_and_sip_external_many.py
+++ broadinstitute_psp/steep_and_sip_external/steep_and_sip_external_many.py
@@ -14,11 +14,12 @@ import logging
 import os
 import sys
 import time
+import traceback
 
 import broadinstitute_cmap.io.pandasGEXpress.parse as pg
 import broadinstitute_cmap.io.pandasGEXpress.write_gct as wg
 
-import broadinstitute_psp.steep_and_sip_external as steep_and_sip_external
+import broadinstitute_psp.steep_and_sip_external.steep_and_sip_external as steep_and_sip_external
 import broadinstitute_psp.utils.setup_logger as setup_logger
 
 __author__ = "Lev Litichevskiy"
@@ -30,9 +31,9 @@ INTERNAL_GCT_FORMAT = "{assay}_{cell}_QCNORM.gct"
 BG_GCT_FORMAT = "{assay}_{cell}_SIM.gct"
 OUT_STEEP_FORMAT = "{cell}_SIM.gct"
 OUT_SIP_FORMAT = "{cell}_CONN.gct"
+OUT_DIR_FORMAT = "steep_and_sip_external_many_{time}"
 
 # TODO(LL): rename this, make this easy for AO's use case (e.g. annotation)?
-# TODO(LL): add a proper test
 # TODO(LL): need to figure out how we will pass -fae through
 
 def build_parser():
@@ -61,6 +62,14 @@ def build_parser():
 
 def main(args):
 
+    # Record start_time
+    start_time = datetime.datetime.now()
+
+    # Create output directory using start_time down to the millisecond
+    time_for_out_dir = start_time.strftime("%Y_%m_%d_%H%M%S%f")
+    actual_out_dir = os.path.join(args.out_dir, OUT_DIR_FORMAT.format(time=time_for_out_dir))
+    os.makedirs(actual_out_dir)
+
     try:
 
         # Read and unpack config file
@@ -76,48 +85,41 @@ def main(args):
 
             # Import gct with the internal profiles for this cell line
             internal_gct_path = os.path.join(internal_gct_dir, INTERNAL_GCT_FORMAT.format(
-                args.assay, cell))
+                assay=args.assay, cell=cell))
             internal_gct = pg.parse(internal_gct_path, convert_neg_666=False, make_multiindex=True)
 
             # Import gct with the similarity matrix for this cell line
             bg_gct_path = os.path.join(bg_gct_dir, BG_GCT_FORMAT.format(
-                args.assay, cell))
+                assay=args.assay, cell=cell))
             bg_gct = pg.parse(bg_gct_path, convert_neg_666=False, make_multiindex=True)
 
-            try:
-                (sim_gct, conn_gct) = steep_and_sip_external.do_steep_and_sip(
-                    external_gct, internal_gct, bg_gct, "spearman",
-                    "ks_test", fields_to_aggregate_for_external_profiles,
-                    fields_to_aggregate_for_internal_profiles)
+            (sim_gct, conn_gct) = steep_and_sip_external.do_steep_and_sip(
+                external_gct, internal_gct, bg_gct, "spearman",
+                "ks_test", fields_to_aggregate_for_external_profiles,
+                fields_to_aggregate_for_internal_profiles)
 
-                # Write output gcts
-                out_steep_name = os.path.join(args.out_dir, OUT_STEEP_FORMAT.format(cell))
-                out_sip_name = os.path.join(args.out_dir, OUT_SIP_FORMAT.format(cell))
-                wg.write(sim_gct, out_steep_name, data_null="NaN", metadata_null="NA", filler_null="NA")
-                wg.write(conn_gct, out_sip_name, data_null="NaN", filler_null="NaN", metadata_null="NaN")
-
-            except Exception as e:
-
-                # TODO(LL): figure out how to get the stacktrace properly
-
-                msg = "Could not complete for cell {}. stacktrace: {}".format(cell, str(e))
-
-                # Write failure.txt
-                write_text_file(os.path.join(args.out_dir, "failure.txt"), msg)
-
-                # Raise exception
-                logger.error(msg)
-                raise Exception(msg)
+            # Write output gcts
+            out_steep_name = os.path.join(actual_out_dir, OUT_STEEP_FORMAT.format(cell=cell))
+            out_sip_name = os.path.join(actual_out_dir, OUT_SIP_FORMAT.format(cell=cell))
+            wg.write(sim_gct, out_steep_name, data_null="NaN", metadata_null="NA", filler_null="NA")
+            wg.write(conn_gct, out_sip_name, data_null="NaN", filler_null="NaN", metadata_null="NaN")
 
         # Write success.txt with timestamp
-        timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        write_text_file(os.path.join(args.out_dir, "success.txt"), timestamp)
+        success_path = os.path.join(actual_out_dir, "success.txt")
+        write_success(success_path)
 
-    except Exception as e:
-        msg = "Something failed outside of looping over cell lines. stacktrace: "
+        # Return how much time it took
+        end_time = datetime.datetime.now()
+        minutes_elapsed = (end_time - start_time).seconds / 60.
+        logger.info("steep_and_sip_external_many.py completed in {:.1f} min.".format(minutes_elapsed))
+
+
+    except Exception:
+        failure_path = os.path.join(actual_out_dir, "failure.txt")
+        msg = "steep_and_sip_external_many.py failed. See {} for stacktrace.".format(failure_path)
 
         # Write failure.txt
-        write_text_file(os.path.join(args.out_dir, "failure.txt"), msg)
+        write_failure(failure_path)
 
         # Raise exception
         logger.error(msg)
@@ -153,9 +155,23 @@ def read_config_file(config_path):
            similarity_metric, connectivity_metric
 
 
-def write_text_file(file_name, file_contents):
+def write_success(file_name):
+    # Create timestamp
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+    # Write timestamp to file_name
     f = open(file_name, 'w')
-    f.write(file_contents)
+    f.write("steep_and_sip_external_many.py completed at {}".format(timestamp))
+    f.close()
+
+
+def write_failure(file_name):
+    # Record stacktrace
+    _, _, exc_traceback = sys.exc_info()
+
+    # Write stacktrace to file_name
+    f = open(file_name, "w")
+    traceback.print_exc(exc_traceback, file=f)
     f.close()
 
 
--- broadinstitute_psp/steep_and_sip_external/test_steep_and_sip_external_many.py
+++ broadinstitute_psp/steep_and_sip_external/test_steep_and_sip_external_many.py
@@ -1,7 +1,8 @@
+import os
 import unittest
 import steep_and_sip_external_many as ssec
 
-# HOWTO: run from broadinstitute_psp directory
+FUNCTIONAL_TESTS_DIR = "steep_and_sip_external/functional_tests"
 
 
 class TestSteepAndSipExternalMany(unittest.TestCase):
@@ -16,6 +17,32 @@ class TestSteepAndSipExternalMany(unittest.TestCase):
         self.assertEqual(fields_to_aggregate_for_external_profiles[1], "cell_id")
         self.assertEqual(connectivity_metric, "ks_test")
 
+    def test_write_failure(self):
+        out_file = os.path.join(FUNCTIONAL_TESTS_DIR, "failure.txt")
+
+        try:
+            tuple()[0]
+        except Exception:
+            ssec.write_failure(out_file)
+
+        self.assertTrue(os.path.exists(out_file))
+        os.remove(out_file)
+
+    def test_write_success(self):
+        out_file = os.path.join(FUNCTIONAL_TESTS_DIR, "success.txt")
+
+        ssec.write_success(out_file)
+
+        self.assertTrue(os.path.exists(out_file))
+        os.remove(out_file)
+
+    def test_main(self):
+        test_external = os.path.join(FUNCTIONAL_TESTS_DIR, "test_steep_and_sip_external_many_single_sample.gct")
+
+        args_string = "-a {} -e {} -o {}".format("GCP", test_external, FUNCTIONAL_TESTS_DIR)
+        args = ssec.build_parser().parse_args(args_string.split())
+
+        ssec.main(args)
 
 if __name__ == '__main__':
     unittest.main()
