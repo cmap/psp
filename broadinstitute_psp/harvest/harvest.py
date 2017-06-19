9ba4eda34e2ab6d33f3a818c16ebe7c454565e7f
lev <lev@broadinstitute.org>
2017-06-19 10:44:58 -0400

    harvest/harvest.py: improvements to get_metadata method

--- broadinstitute_psp/harvest/harvest.py
+++ broadinstitute_psp/harvest/harvest.py
@@ -1,95 +1,128 @@
+"""
+
+On Panorama:
+1. Admin > Go to module > Query
+2. Click on targetedms folder
+3. couldn't figure out how to display data...hmmph
+
+Result of query of 'replicateannotation' with one RunId:
+- dict with length 7; keys = ['rows', 'queryName', 'rowCount', 'formatVersion', 'columnModel', 'schemaName', 'metaData']
+- len(dict["rows"]) = 2112, this is a list of 2112 things
+- Each of those things is a dict:
+    rep_query_res["rows"][0]
+    {u'ReplicateId/Name': u'A01_acq_02', u'Name': u'id', u'ReplicateId/Id': 1079098, u'Value': u'GY1-33848-001A01', u'Id': 563487, u'ReplicateId/RunId/Id': 16355}
+
+    rep_query_res["rows"][3]
+    {u'ReplicateId/Name': u'A01_acq_02', u'Name': u'det_well', u'ReplicateId/Id': 1079098, u'Value': u'A1', u'Id': 563490, u'ReplicateId/RunId/Id': 16355}
+
+- So it's like the column metadata was rolled out.
+
+To reassemble...
+- 'Name' will become the metadata header
+- 'Value' will become whatever fills the metadata header
+- 'ReplicateId/Name' (or 'ReplicateId/Id' will probably be better) is what
+we'll use to figure out which rows (i.e. entries in the list of 2112)
+belong together
+
+
+Result of query of 'generalmoleculeannotation' with one RunId is similar.
+- dict with length 7; keys = ['rows', 'queryName', 'rowCount', 'formatVersion', 'columnModel', 'schemaName', 'metaData']
+- len(dict["rows"]) = 17113, this is a list of 17113 things
+- Each of those things is a dict:
+    peptide_query_res["rows"][0]
+    {u'Name': u'pr_id', u'PeptideId/Id': 561546, u'PeptideId/PeptideModifiedSequence': u'T[+56.0]K[+56.0]QTAR', u'Value': u'BI10003', u'_labkeyurl_PeptideId/PeptideGroupId/Label': u'/labkey/targetedms/LINCS/GCP/showProtein.view?id=119057', u'_labkeyurl_PeptideId/Sequence': u'/labkey/targetedms/LINCS/GCP/showPeptide.view?id=561546', u'PeptideId/PeptideGroupId/RunId/File/Id': 3030, u'PeptideId/Sequence': u'TKQTAR', u'PeptideId/PeptideGroupId/Label': u'H3K4me0 - BI10003', u'Id': 438782}
+
+    peptide_query_res["rows"][4]
+    {u'Name': u'pr_gcp_base_peptide', u'PeptideId/Id': 561546, u'PeptideId/PeptideModifiedSequence': u'T[+56.0]K[+56.0]QTAR', u'Value': u'TKQTAR', u'_labkeyurl_PeptideId/PeptideGroupId/Label': u'/labkey/targetedms/LINCS/GCP/showProtein.view?id=119057', u'_labkeyurl_PeptideId/Sequence': u'/labkey/targetedms/LINCS/GCP/showPeptide.view?id=561546', u'PeptideId/PeptideGroupId/RunId/File/Id': 3030, u'PeptideId/Sequence': u'TKQTAR', u'PeptideId/PeptideGroupId/Label': u'H3K4me0 - BI10003', u'Id': 438786}
+
+- So it's like the row metadata was rolled out.
+
+To reassemble...
+- 'Name' will become the metadata header
+- 'Value' will become whatever fills the metadata header
+- 'ReplicateId/Name' (or 'ReplicateId/Id' will probably be better) is what
+we'll use to figure out which rows (i.e. entries in the list of 2112)
+belong together
+
+Note that in this case, the fields will be different for GCP and P100 (i.e.
+different RunIds will have different metadata headers). That's okay, I guess.
+
+
+We can pull the GCT from Skyline, but we can also pull annotation and data
+separately and assemble it ourselves.
+
+To pull the GCTs, we can see what Skyline files are in the 'runs' table,
+parse those strings to figure out what the download URL for the unprocessed
+GCT is, and download the GCT.
+
+"""
+
+# TODO(LL): Incomplete! Need to decide if we want to pull GCTs
+# or assemble GCTs after pulling metadata.
+
+import datetime
 import logging
 import labkey
-import utils.setup_logger as setup_logger
+import pandas as pd
 import urllib
 import os
+import broadinstitute_psp.utils.setup_logger as setup_logger
 
 logger = logging.getLogger(setup_logger.LOGGER_NAME)
 
-assay_type = "GCP"
-sql_table = "runs"
+LABKEY_SERVER = "panoramaweb.org"
+PROJECT_NAME_BASE = "LINCS/"
+CONTEXT_PATH = "labkey"
+SCHEMA = "targetedms"
+SKY_FILES_TABLE = "runs"
 
-labkey_server = "panoramaweb.org"
-project_name_base = "LINCS/"
-context_path = "labkey"
-schema = "targetedms"
+ROW_METADATA_TABLE = "generalmoleculeannotation"
+ROW_METADATA_VIEW = "GCT_peptide_annotation"
+ROW_METADATA_RUN_ID_PREFIX = "PeptideId/PeptideGroupId/RunId/File/Id"
 
-# TODO(lev): refactor. There should be get_metadata and get_data methods.
+COL_METADATA_TABLE = "replicateannotation"
+COL_METADATA_VIEW = "GCT_replicate_annotation"
+COL_METADATA_RUN_ID_PREFIX = "ReplicateId/RunId/Id"
 
 
-def get_metadata(assay_type, run_id):
+def get_metadata(assay_type, run_id, run_id_prefix, table, view):
     """ Extract metadata corresponding to a particular run_id (i.e. a
     single plate).
 
     Args:
         assay_type (string): choices = {"GCP", "P100"}
         run_id (int)
+        run_id_prefix (string): depends on the table being queried
+        table (string): name of SQL table
+        view (string): name of table view
 
     Returns:
-        query_result (list of dicts): len = #rows x #row_metadata_headers;
-            keys of each dict entry correspond to metadata headers, values
-            correspond to the actual metadata
+        query_result (dict): w/ 7 keys. query_result["rows"] is a list with
+            length N where each entry is one metadata value; this result
+            is like the metadata df rolled out
 
     """
     # Create server context
-    project_name = project_name_base + assay_type
-    table = "generalmoleculeannotation"
-    view = "GCT_peptide_annotation"
+    project_name = PROJECT_NAME_BASE + assay_type
     server_context = labkey.utils.create_server_context(
-        labkey_server, project_name, context_path, use_ssl=True)
+        LABKEY_SERVER, project_name, CONTEXT_PATH, use_ssl=True)
 
     # Only get results for a particular run_id
-    qf = labkey.query.QueryFilter("PeptideId/PeptideGroupId/RunId/File/Id",
-                                  run_id)
+    qf = labkey.query.QueryFilter(run_id_prefix, run_id)
 
     # Make the SQL request
-    result = labkey.query.select_rows(
-        server_context, schema, table, filter_array=[qf], view_name=view)
+    query_result = labkey.query.select_rows(
+        server_context, SCHEMA, table, filter_array=[qf],
+        view_name=view)
 
     # Return the files that were found
-    if len(result["rows"]) > 1:
-        logger.info("result['rows'][0]: {}".format(result["rows"][0]))
-        logger.info("Number of rows returned: " + str(result["rowCount"]))
+    if len(query_result["rows"]) > 1:
+        logger.info("result['rows'][0]: {}".format(query_result["rows"][0]))
+        logger.info("Number of rows returned: " + str(query_result["rowCount"]))
     else:
-       logger.error("Failed to load results from " + schema + "." + table)
+        logger.error("Failed to load results from " + SCHEMA + "." + table)
 
-
-def connect_to_col_metadata(assay_type):
-    """ Get list of all Skyline files on Panorama for a certain assay type.
-
-    Args:
-        assay_type (string): choices = {"GCP", "P100"}
-        run_id (int)
-
-    Returns:
-        query_result (list of dicts): len = #rows x #row_metadata_headers;
-            keys of each dict entry correspond to metadata headers, values
-            correspond to the actual metadata
-
-    """
-    # Create server context
-    project_name = project_name_base + assay_type
-    table = "generalmoleculeannotation"
-    view = "GCT_peptide_annotation"
-    server_context = labkey.utils.create_server_context(
-        labkey_server, project_name, context_path, use_ssl=True)
-
-    # Only get results for a particular run_id
-    qf = labkey.query.QueryFilter("PeptideId/PeptideGroupId/RunId/File/Id",
-                                  run_id)
-
-    # Make the SQL request
-    result = labkey.query.select_rows(
-        server_context, schema, table, filter_array=[qf], view_name=view)
-
-    # Return the files that were found
-    if len(result["rows"]) > 1:
-        logger.info("result['rows'][0]: {}".format(result["rows"][0]))
-        logger.info("Number of rows returned: " + str(result["rowCount"]))
-    else:
-       logger.error("Failed to load results from " + schema + "." + table)
-
-        # ReplicateId/RunId/Id
+    return query_result
 
 
 def get_skyline_files(assay_type):
@@ -100,22 +133,22 @@ def get_skyline_files(assay_type):
         assay_type (string)
 
     Returns:
-        sky_files (list of strings)
+        sky_files (list of strings): each string is the name of a zipped
+            .sky file
 
     """
 
     # Create server context
-    project_name = project_name_base + assay_type
-    table = "runs"
+    project_name = PROJECT_NAME_BASE + assay_type
     server_context = labkey.utils.create_server_context(
-        labkey_server, project_name, context_path, use_ssl=True)
+        LABKEY_SERVER, project_name, CONTEXT_PATH, use_ssl=True)
 
     # Exclude Skyline files that were unsuccessfully uploaded
     qf = labkey.query.QueryFilter("Status", "")
 
     # Make the SQL query
     result = labkey.query.select_rows(
-        server_context, schema, table, filter_array=[qf])
+        server_context, SCHEMA, SKY_FILES_TABLE, filter_array=[qf])
 
     # Extract the Skyline files
     if len(result["rows"]) > 1:
@@ -123,9 +156,28 @@ def get_skyline_files(assay_type):
         logger.debug("sky_files:\n{}".format(sky_files))
         logger.info("Number of rows returned: " + str(result["rowCount"]))
     else:
-       logger.error("Failed to load results from " + schema + "." + table)
+        logger.error("Failed to load results from " + SCHEMA + "." + table)
 
     return sky_files
+
+
+def create_sky_files_log(sky_files, dir_w_log_files):
+
+    # Make the list into a dataframe
+    sky_file_df = pd.DataFrame(sky_files)
+
+    # Write to file with timestamp
+    now_time = datetime.datetime.now()
+
+
+    # TODO(LL): CONTINUE HERE!
+
+
+    actual_out_name = os.path.join(dir_w_log_files, out_name)
+
+
+
+
 
 
 def create_urls_from_skyline_files(assay_type, sky_files, suffix):
@@ -136,6 +188,7 @@ def create_urls_from_skyline_files(assay_type, sky_files, suffix):
     logger.debug("urls: {}".format(urls))
 
     return urls
+
 
 def download_urls(urls, out_dir):
 
@@ -153,11 +206,11 @@ def download_urls(urls, out_dir):
 def get_run_ids(wildcard):
     pass
 
-setup_logger.setup(verbose=False)
+# setup_logger.setup(verbose=False)
 # connect_to_row_metadata("GCP", 3030)
 
-assay_type = "P100"
-
-skyline_files = get_skyline_files(assay_type)
-urls = create_urls_from_skyline_files(assay_type, skyline_files, ".gct")
-download_urls(urls, "/cmap/data/proteomics/harvest/wget_unprocessed")
+# assay_type = "P100"
+#
+# skyline_files = get_skyline_files(assay_type)
+# urls = create_urls_from_skyline_files(assay_type, skyline_files, ".gct")
+# download_urls(urls, "/cmap/data/proteomics/harvest/wget_unprocessed")
